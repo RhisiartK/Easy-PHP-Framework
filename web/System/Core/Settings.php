1ba69137add8c8145c2111c9e54b35e51c8a1116
Richard Keki <kricsi14@gmail.com>
2018-02-19 16:01:27 +0100

    changed settings' constants (removed public accessibility because of phpdocumentor parse error)

--- web/System/Core/Settings.php
+++ web/System/Core/Settings.php
@@ -1,17 +1,26 @@
 <?php
 declare(strict_types=1);
 
-
 namespace System\Core;
 
-
 /**
- * Class Settings
+ * Settings of web page and framework
+ *
  * @package System\Core
  */
 class Settings
 {
-    public const DB_DSN      = 'mysql:host=localhost;dbname=DATABASE_NAME;charset=utf8_bin';
-    public const DB_USERNAME = 'DATABASE_USER';
-    public const DB_PASSWORD = 'DATABASE_PASSWORD';
+
+    /**
+     * The Data Source Name, or DSN, contains the information required to connect to the database.
+     */
+    const DB_DSN      = 'mysql:host=localhost;dbname=DATABASE_NAME;charset=utf8_bin';
+    /**
+     * The user name for the DSN string.
+     */
+    const DB_USERNAME = 'DATABASE_USER';
+    /**
+     * The password for the DSN string
+     */
+    const DB_PASSWORD = 'DATABASE_PASSWORD';
 }
