4619c40e64727a20986ce86cc650cde2abd5ba6a
Richárd Kéki <kricsi14@gmail.com>
2018-02-28 18:29:33 +0100

    start application after set utf8

--- web/Public/index.php
+++ web/Public/index.php
@@ -1,6 +1,23 @@
 <?php
+declare(strict_types=1);
+
+/**
+ * index.php file.
+ *
+ * @author Richard Keki <kricsi14@gmail.com>
+ * @link https://github.com/RhisiartK/Easy-PHP-Framework
+ * @license https://github.com/RhisiartK/Easy-PHP-Framework/blob/master/LICENSE
+ */
+
+use EasyPHP\Core\Settings;
 
 mb_internal_encoding('UTF-8');
 mb_http_output('UTF-8');
+
+include __DIR__ . '/../EasyPHP/Core/Settings.php';
+include Settings::APPLICATION_PATH . 'System/Core/Application.php';
+
+new EasyPHP\Core\Application();
+
 ?>
 Index.php
