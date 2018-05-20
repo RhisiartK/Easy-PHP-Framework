c0a5e2a341199279812a26739d1afd26c50d9e81
Richárd Kéki <kricsi14@gmail.com>
2018-05-20 22:05:45 +0200

    policiesAccepted value object added (GDPR update)

---
+++ web/EasyPHP/Validators/PoliciesAccepted.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+/**
+ * PoliciesAccepted.php class file.
+ *
+ * @author Richard Keki <kricsi14@gmail.com>
+ * @link https://github.com/RhisiartK/Easy-PHP-Framework
+ * @license https://github.com/RhisiartK/Easy-PHP-Framework/blob/master/LICENSE
+ */
+
+namespace EasyPHP\Validators;
+
+use EasyPHP\Interfaces\IValidator;
+
+/**
+ * Class PoliciesAccepted
+ * @package EasyPHP\Validators
+ */
+class PoliciesAccepted implements IValidator
+{
+    /**
+     *  Check policies accepted is valid (bool)
+     *
+     * @param $value
+     * @return bool
+     */
+    public function isValid($value): bool
+    {
+        $options = [
+            'options' => [
+                'default' => null,
+            ],
+            'flags'   => 'FILTER_NULL_ON_FAILURE',
+        ];
+
+        return (\is_bool($value) || \is_string($value) || \is_int($value))
+            && filter_var($value, FILTER_VALIDATE_BOOLEAN, $options) !== null;
+    }
+}
---
+++ web/EasyPHP/ValueObjects/PoliciesAccepted.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+/**
+ * PoliciesAccepted.php class file.
+ *
+ * @author Richard Keki <kricsi14@gmail.com>
+ * @link https://github.com/RhisiartK/Easy-PHP-Framework
+ * @license https://github.com/RhisiartK/Easy-PHP-Framework/blob/master/LICENSE
+ */
+
+namespace EasyPHP\ValueObjects;
+
+use EasyPHP\Core\ErrorCodes;
+use EasyPHP\Core\ValueObject;
+use EasyPHP\Interfaces\IValidator;
+use EasyPHP\Validators\PoliciesAccepted as PoliciesAcceptedValidator;
+
+class PoliciesAccepted extends ValueObject
+{
+    /**
+     * @var ?bool
+     */
+    private $value;
+
+    /**
+     * PoliciesAccepted constructor.
+     * @param string|bool|int|null $value
+     * @param IValidator|null $validator
+     */
+    public function __construct($value, IValidator $validator = null)
+    {
+        $this->validator = $validator ?? new PoliciesAcceptedValidator();
+
+        if ($value !== null) {
+            $this->set($value);
+        } else {
+            $this->errorCode = ErrorCodes::VALUE_OBJECT_NOT_VALID;
+        }
+    }
+
+    /**
+     * @param string|bool|int $value
+     * @return void
+     */
+    private function set($value): void
+    {
+        if ($this->validator->isValid($value) === true) {
+            $this->value = (bool)$value;
+            return;
+        }
+
+        $this->value     = null;
+        $this->errorCode = ErrorCodes::VALUE_OBJECT_NOT_VALID;
+    }
+
+    public function get(): ?bool
+    {
+        return $this->value;
+    }
+}
