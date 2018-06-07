5097977075b76b63eff72daa9cd91e6df4fec143
Richárd Kéki <kricsi14@gmail.com>
2018-06-08 01:10:27 +0200

    PDO automatically use php7 types

--- web/EasyPHP/Core/Database.php
+++ web/EasyPHP/Core/Database.php
@@ -51,10 +51,11 @@ class Database extends PDO
             Settings::DB_USERNAME,
             Settings::DB_PASSWORD,
             [
-                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4_bin'",
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4'",
                 PDO::MYSQL_ATTR_FOUND_ROWS => true,
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_PERSISTENT => false
+                PDO::ATTR_PERSISTENT => false,
+                PDO::ATTR_EMULATE_PREPARES => false
             ]
         );
     }
