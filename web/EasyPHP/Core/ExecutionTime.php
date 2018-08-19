a9a331a53fd0a37bd007c993d4c6610de8e9c681
RhisiartK <kricsi14@gmail.com>
2018-08-19 16:14:45 +0000

    Apply fixes from StyleCI

--- web/EasyPHP/Core/ExecutionTime.php
+++ web/EasyPHP/Core/ExecutionTime.php
@@ -1,10 +1,13 @@
 <?php
+
 declare(strict_types=1);
 /**
  * ExecutionTime.php class file.
  *
  * @author  Richard Keki <kricsi14@gmail.com>
+ *
  * @link    https://github.com/RhisiartK/Easy-PHP-Framework
+ *
  * @license https://github.com/RhisiartK/Easy-PHP-Framework/blob/master/LICENSE
  */
 
@@ -13,6 +16,7 @@ namespace EasyPHP\Core;
 class ExecutionTime
 {
     private $startTime;
+
     private $endTime;
 
     public function start():void
@@ -27,8 +31,8 @@ class ExecutionTime
 
     private function runTime($ru, $rus, $index): int
     {
-        return ($ru["ru_$index.tv_sec"] * 1000 + (int)($ru["ru_$index.tv_usec"] / 1000))
-            - ($rus["ru_$index.tv_sec"] * 1000 + (int)($rus["ru_$index.tv_usec"] / 1000));
+        return ($ru["ru_$index.tv_sec"] * 1000 + (int) ($ru["ru_$index.tv_usec"] / 1000))
+            - ($rus["ru_$index.tv_sec"] * 1000 + (int) ($rus["ru_$index.tv_usec"] / 1000));
     }
 
     public function toString(): string
